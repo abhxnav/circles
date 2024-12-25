1867ca8e1037a1ad86bad82f69ec682698b5a01b
abhxnav <k.abhinav1000@gmail.com>
2024-12-26 03:14:58 +0530

    feat : add sidebar for desktop and bottom-bar for mobile screens

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -1,5 +1,30 @@
+import { bottombarLinks } from '@/constants'
+import { NavLink, useLocation } from 'react-router-dom'
+
 const BottomBar = () => {
-  return <div className="text-white">BottomBar</div>
+  const { pathname } = useLocation()
+
+  return (
+    <section className="z-50 flex items-center justify-around w-full sticky bottom-0 bg-dark-secondary py-2 md:hidden">
+      {bottombarLinks.map((link: NavLink) => {
+        const isActive = pathname === link.route
+
+        return (
+          <NavLink
+            key={link.label}
+            to={link.route}
+            className={`rounded-full flex flex-col items-center gap-2 group p-1 transition`}
+          >
+            <img
+              src={link.iconUrl}
+              alt={link.label}
+              className={`size-5 ${!isActive && 'filter-white'}`}
+            />
+          </NavLink>
+        )
+      })}
+    </section>
+  )
 }
 
 export default BottomBar
